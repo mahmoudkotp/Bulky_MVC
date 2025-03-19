c36c30a1f15e7ddd15016bf6034d05e7ce686519
MahmoudKotp <Mahmoud.hassan.Kotp@gmail.com>
2025-03-19 11:16:53 +0200

    pushModel

---
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,113 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.DataAccess.Data;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class ProductController : Controller
+	{
+		// I fixed this error with changing the access modifier from internal
+		// to public in the IProductRepository interface.
+		private readonly IUnitOfWork _unitOfWork;
+
+		public ProductController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		public IActionResult Index()
+		{
+			List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+			
+			return View(objProductList);
+		}
+
+		public IActionResult Create()
+		{
+			IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+			{
+				Text = u.Name,
+				Value = u.Id.ToString(),
+			});
+			// Passing to View
+			//ViewBag.Category = CategoryList;
+			ViewData["CategoryList"] = CategoryList;
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Create(Product obj)
+		{
+			
+			if (ModelState.IsValid)
+			{
+				_unitOfWork.Product.Add(obj);
+				_unitOfWork.Save();
+				TempData["success"] = "Product created successfully";
+				// Go back to the index page (Product)
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+		public IActionResult Edit(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Product productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+			if (productFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(productFromDb);
+		}
+
+		[HttpPost]
+		public IActionResult Edit(Product obj)
+		{
+			if (ModelState.IsValid)
+			{
+				_unitOfWork.Product.Update(obj);
+				_unitOfWork.Save();
+				TempData["success"] = "Product updated successfully";
+				// Go back to the index page (Product)
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Product productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+			if (productFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(productFromDb);
+		}
+
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePost(int? id)
+		{
+
+			Product? obj = _unitOfWork.Product.Get(u => u.Id == id);
+			if (obj == null)
+			{
+				return NotFound();
+			}
+			_unitOfWork.Product.Remove(obj);
+			_unitOfWork.Save();
+			TempData["success"] = "Product deleted successfully!";
+			return RedirectToAction("Index");
+		}
+	}
+}
--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,7 +1,20 @@
+using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository;
+using BulkyBook.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//  ”ÃÌ· DbContext „⁄ SQL Server
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Registration of Repository
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
 
 var app = builder.Build();
 
@@ -22,8 +35,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}"
+);
 
 app.Run();
